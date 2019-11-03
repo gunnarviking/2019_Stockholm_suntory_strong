a1fc6e7c1a7317c3cb8bd33a00a0f0bc7fe3e1a7
Michael <micgtho@gmail.com>
2019-11-03 13:25:34 +0100

    updated two files

--- app/common/data.py
+++ app/common/data.py
@@ -10,15 +10,18 @@ def add_path_to_image(data):
     category = data[0]["category"]["category"]
     if category == "Polishändelse":
         img_path = img_police
+        classCss = "police"
     else:
         img_path = img_news
+        classCss = "news"
 
     for entry in data:
         entry["path_image"] = img_path
+        entry["classCss"] = classCss
     
 
 def get_entries():
-    path_json_files_directory = "../data"
+    path_json_files_directory = "../data/processed"
     path_json_files = [f for f in listdir(path_json_files_directory) if isfile(join(path_json_files_directory, f))]
     path_json_files = [ join(path_json_files_directory, path) for path in path_json_files]
     combinedData = []
@@ -40,6 +43,10 @@ def sort_dates_descending(data):
 def add_pretty_date(data):
      for entry in data:
         date_time_str = entry["datetime"]
+        if isinstance(date_time_str, str) == False:
+            date_time_str = entry["datetime"]["datetime"]
+
+        print(date_time_str)
         if "+" in date_time_str and "T" in date_time_str:
             date_time_obj = datetime.datetime.strptime(date_time_str, '%Y-%m-%dT%H:%M:%S%z')
         elif "+" in date_time_str:
@@ -48,6 +55,3 @@ def add_pretty_date(data):
             date_time_obj = datetime.datetime.strptime(date_time_str, '%Y-%m-%dT%H:%M:%SZ')
         date_time_obj_pretty = date_time_obj.strftime('%b %d %H:%M')
         entry["datetime_pretty"] = date_time_obj_pretty
-        print(entry)
-
-get_entries()
